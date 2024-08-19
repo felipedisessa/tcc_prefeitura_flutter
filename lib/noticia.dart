12a32193e7f8506b970b2262e6d4765d60defc58
felipedisessa <felipetmds@hotmail.com>
2024-08-19 18:02:54 -0300

    WIP

--- lib/noticia.dart
+++ lib/noticia.dart
@@ -2,15 +2,21 @@ class Noticia {
   final int id;
   final String name;
   final String description;
+  final String? imageUrl; // Novo campo para a URL da imagem
 
-
-  Noticia({required this.id, required this.name, required this.description});
+  Noticia({
+    required this.id,
+    required this.name,
+    required this.description,
+    this.imageUrl,
+  });
 
   factory Noticia.fromJson(Map<String, dynamic> json) {
     return Noticia(
       id: json['id'],
       name: json['name'],
       description: json['description'],
+      imageUrl: json['image_url'], // Acessa o campo da imagem
     );
   }
 }
